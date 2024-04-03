1b442803a9a03a6dfbd2a021458ff3c5920d5d47
woody <gelius24@hotmail.com>
2024-04-03 01:05:41 -0400

    .. ..

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.URL }));
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
